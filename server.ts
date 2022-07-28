4318eee05f042de259f1827de90fbdd6917330ae
ruanxihao <ruanxihao@xiaomi.com>
2022-07-28 22:09:25 +0800

    fix: json demo

--- server.ts
+++ server.ts
@@ -6,14 +6,29 @@ export default {
     const url = new URL(request.url)
 
     const flowersRes = (opt = {}) => new Response(Bun.file("flowers.jpg"), opt)
+    const jsonRes = (headers = {}) =>
+      new Response(JSON.stringify({ name: "flowser" }), {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          ...headers,
+        },
+      })
 
-    if (url.pathname === "/") {
+    if (url.pathname === "/img") {
       return flowersRes()
-    } else if (url.pathname === "/download") {
+    } else if (url.pathname === "/img/download") {
       return flowersRes({
         headers: {
           "Content-Disposition": 'attachment; filename="filename.jpg"',
         },
+      })
+    }
+
+    if (url.pathname === "/json") {
+      return jsonRes()
+    } else if (url.pathname === "/json/download") {
+      return jsonRes({
+        "Content-Disposition": 'attachment; filename="filename.json"',
       })
     }
   },
