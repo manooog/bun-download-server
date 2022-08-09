87b40efd622223feed83adffe444a2080976787a
ruanxihao <ruanxihao@xiaomi.com>
2022-08-09 10:57:21 +0800

    fix: 完善示例内容

--- server.ts
+++ server.ts
@@ -1,25 +1,25 @@
+const fileRes = (path = "", opt = {}) => new Response(Bun.file(path), opt)
+const jsonRes = (headers = {}) =>
+  new Response(JSON.stringify({ name: "flowser" }), {
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      ...headers,
+    },
+  })
+
 // http.js
 export default {
-  // ⚠️ 3000 端口如果被占用的话，程序执行会出错，并且没有一个明确的错误提示。
-  port: 3000,
+  // ⚠️ 3005 端口如果被占用的话，程序执行会出错，并且没有一个明确的错误提示。
+  port: 3005,
   fetch(request: Request) {
     const url = new URL(request.url)
 
-    const flowersRes = (opt = {}) => new Response(Bun.file("flowers.jpg"), opt)
-    const jsonRes = (headers = {}) =>
-      new Response(JSON.stringify({ name: "flowser" }), {
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          ...headers,
-        },
-      })
-
     if (url.pathname === "/img") {
-      return flowersRes()
+      return fileRes("flowers.jpeg")
     } else if (url.pathname === "/img/download") {
-      return flowersRes({
+      return fileRes("flowers.jpeg", {
         headers: {
-          "Content-Disposition": 'attachment; filename="filename.jpg"',
+          "Content-Disposition": 'attachment; filename="flowers.jpeg"',
         },
       })
     }
@@ -28,8 +28,12 @@ export default {
       return jsonRes()
     } else if (url.pathname === "/json/download") {
       return jsonRes({
-        "Content-Disposition": 'attachment; filename="filename.json"',
+        "Content-Disposition": 'attachment; filename="flowers.json"',
       })
+    }
+
+    if (url.pathname === "/download") {
+      return fileRes("index.html")
     }
   },
 }
